5ea9a0c9c9895de59e5357eba644a89ba5f99072
Robbert van Dalen <robbert@eigentime.nl>
2026-05-05 21:22:31 +0200

    maven support

--- src/main/java/org/jatari/main/Start.java
+++ src/main/java/org/jatari/main/Start.java
@@ -6,17 +6,15 @@ public class Start {
     public static void main(String[] args) {
         var ym2149 = new Ym2149AudioIndexed();
         ym2149.applyReset();
-        ym2149.setTonePeriod(0, 0x123);
-        ym2149.setTonePeriod(1, 0x456);
-        ym2149.setTonePeriod(2, 0x789);
+        ym2149.setNoisePeriod(5);
         ym2149.setVolume(0, 15);
         ym2149.setVolume(1, 7);
         ym2149.setVolume(2, 3);
-        ym2149.setMixer(true, true, true, false, false, false);
+        ym2149.setMixer(false, false, false, true, true, true);
         
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 1_000_000_000; i++) {
             ym2149.risingEdge(true, false, true, false, false, 0);
-            if ((i % 100) == 0) {
+            if ((i % 100_000_000) == 0) {
                 System.out.printf("Sample %d: chA=%d chB=%d chC=%d %n",
                         i, ym2149.getChAIndexO(), ym2149.getChBIndexO(), ym2149.getChCIndexO());
             }
