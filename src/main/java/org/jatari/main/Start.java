2dd749c9f29c9ab590c5e71272af9888e06d4a05
Robbert van Dalen <robbert@eigentime.nl>
2026-05-09 16:16:56 +0200

    jaust dependency

--- src/main/java/org/jatari/main/Start.java
+++ src/main/java/org/jatari/main/Start.java
@@ -8,13 +8,14 @@ public class Start {
         ym2149.applyReset();
         ym2149.setNoisePeriod(5);
         ym2149.setVolume(0, 15);
-        ym2149.setVolume(1, 7);
-        ym2149.setVolume(2, 3);
+        //ym2149.setVolume(1, 7);
+        //ym2149.setVolume(2, 3);
         ym2149.setMixer(false, false, false, true, true, true);
         
         for (int i = 0; i < 1_000_000_000; i++) {
             ym2149.risingEdge(true, false, true, false, false, 0);
             if ((i % 100_000_000) == 0) {
+                System.out.println(i);
                 System.out.printf("Sample %d: chA=%d chB=%d chC=%d %n",
                         i, ym2149.getChAIndexO(), ym2149.getChBIndexO(), ym2149.getChCIndexO());
             }
